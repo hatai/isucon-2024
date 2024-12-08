c4ad0e4ed1df9f78fd698fc4158fc62d9bb2683c
taigen.hamada <taigen.hamada@icare-carely.co.jp>
2024-12-08 17:22:54 +0900

    ride_statuses にインデックス追加

--- sql/1-schema.sql
+++ sql/1-schema.sql
@@ -108,7 +108,8 @@ CREATE TABLE ride_statuses
   app_sent_at     DATETIME(6)                                                                NULL COMMENT 'ユーザーへの状態通知日時',
   chair_sent_at   DATETIME(6)                                                                NULL COMMENT '椅子への状態通知日時',
   PRIMARY KEY (id),
-  INDEX idx_ride_statuses_ride_id_creat (ride_id, created_at)
+  INDEX idx_ride_statuses_ride_id_creat (ride_id, created_at DESC),
+  INDEX idx_ride_appsent_created (ride_id, app_sent_at, created_at)
 )
   COMMENT = 'ライドステータスの変更履歴テーブル';
 
