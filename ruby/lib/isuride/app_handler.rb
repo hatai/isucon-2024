2dc48545908ec229d58ce3004f16b20e9ff6d6b0
taigen.hamada <taigen.hamada@icare-carely.co.jp>
2024-12-08 17:30:37 +0900

    chairs にインデックス追加

--- ruby/lib/isuride/app_handler.rb
+++ ruby/lib/isuride/app_handler.rb
@@ -400,13 +400,9 @@ module Isuride
         end
 
       response = db_transaction do |tx|
-        chairs = tx.query('SELECT * FROM chairs')
+        chairs = tx.query('SELECT * FROM chairs WHERE is_active = 1')
 
-        nearby_chairs = chairs.filter_map do |chair|
-          unless chair.fetch(:is_active)
-            next
-          end
-
+        nearby_chairs = chairs.map do |chair|
           rides = tx.xquery('SELECT * FROM rides WHERE chair_id = ? ORDER BY created_at DESC LIMIT 1', chair.fetch(:id))
 
           skip = false
