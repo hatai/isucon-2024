4f351f179f9320ac590c420779281ec7f4d6208a
taigen.hamada <taigen.hamada@icare-carely.co.jp>
2024-12-08 16:53:28 +0900

    app handler の get_chair_stats の select 修正

--- sql/1-schema.sql
+++ sql/1-schema.sql
@@ -91,8 +91,8 @@ CREATE TABLE rides
   created_at            DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '要求日時',
   updated_at            DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '状態更新日時',
   PRIMARY KEY (id),
-  INDEX idx_rides_chair_id ON rides(chair_id),
-  INDEX idx_rides_updated_at ON rides(updated_at)
+  INDEX idx_rides_chair_id (chair_id),
+  INDEX idx_rides_updated_at (updated_at)
 )
   COMMENT = 'ライド情報テーブル';
 
