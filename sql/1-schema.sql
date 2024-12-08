9f28afcd196f3f9e07b4ddd5e6dad3915c0d7013
taigen.hamada <taigen.hamada@icare-carely.co.jp>
2024-12-08 16:34:38 +0900

    app handler の calculate_discounted_fare の select 修正

--- sql/1-schema.sql
+++ sql/1-schema.sql
@@ -132,7 +132,7 @@ CREATE TABLE coupons
   created_at DATETIME(6)  NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '付与日時',
   used_by    VARCHAR(26)  NULL COMMENT 'クーポンが適用されたライドのID',
   PRIMARY KEY (user_id, code),
-  INDEX idx_used_by (used_by),
+  INDEX idx_coupons_used_by (used_by),
   INDEX idx_coupons_user_code_used  (user_id, code, used_by),
   INDEX idx_coupons_user_used_created  (user_id, used_by, created_at)
 )
